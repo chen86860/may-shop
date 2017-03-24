ff24a5a7fade6d6737b5dc7c62924ae956b39e2f
chen86860 <chen86860@126.com>
2017-03-24 22:11:37 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,15 +1,49 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
+
+
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import vueResource from 'vue-resource'
 
-Vue.config.productionTip = false
+import util from './util.js'
+import router from './router.js'
 
-/* eslint-disable no-new */
+import AwesomeSwiper from 'vue-awesome-swiper'
+import {
+  InfiniteScroll,
+  Lazyload
+} from 'mint-ui'
+
+import Pageload from './components/pageLoad.vue'
+import '../src/assets/css/app.css'
+
+// 引入插件
+Vue.use(Lazyload, {
+  preLoad: 1.3,
+  attempt: 1
+}) // 延迟加载 需要进一步处理
+Vue.use(AwesomeSwiper) // 轮播
+Vue.use(InfiniteScroll) // 无限滚动
+Vue.use(vueResource) // ajax插件
+Vue.component(Pageload.name, Pageload)
+
+window.util = util
+
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: `<transition name='fade' mode='out-in'><router-view></router-view></transition>`
 })
